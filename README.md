c702b5ede5fd3185ab4c5e24fe25a2db01ce5f56
lbmann <l.bmann@yahoo.com>
2020-12-17 06:42:30 -0500

    adding index.js that successfully writes to readme.md file and package.json files

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# README.Generator
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    # README.md Generator
+    Generates a README.md file through the terminal command line using Node.js!
+    ## Table of Contents
+    [Installing/Dependencies] (#installing/dependencies)
+    [Walkthrough/Usage Information] (#installing/dependencies)
+    [Contributing to the README.md Generator] (#contributing-to-the-readme.md-generator)
+    [Test Instructions] (#test-instructions)
+    [Author] (#author)
+    [License] (#license)
+    ## Installing/Dependencies
+    ```npm install inquirer```
+    ## Walkthrough/Usage Information
+    run node index.js in the repository of your choice!
+    ## Contributing to the README.md Generator
+    Any recommended changes are welcome, find my contact details below!
+    ## Test Instructions
+    No current testing for this application available.
+    ## Author
+    [Laura Baumann](https://github.com/thelbaumann) -- You can reach me anytime for questions or collaboration at l.bmann@yahoo.com.
+    ## License
+    This project is licensed under [MIT](LICENSE) - 2020 Laura Baumann
---
+++ index.js
@@ -0,0 +1,105 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const licenseImage = new Map([
+    ["Apache", "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"],
+    ["MIT", "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"],
+    ["Creative Commons", "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)"],
+    ["GPLv2", "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"]
+]);
+
+inquirer
+  .prompt([
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "What is the description of your project?",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What are the installation instructions for your project?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "Is there any usage information for your project?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "What are the contribution guidelines for your project?",
+        name: "contribution",
+    },
+    {
+        type: "input",
+        message: "What are the test instructions for your project?",
+        name: "test",
+    },
+    {
+        type: "list",
+        message: "Which license did you choose for your application?",
+        name: "license",
+        choices: ["Apache", "MIT", "Creative Commons", "GPLv2"]
+    },
+    {
+        type: "input",
+        message: "What is your full name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What email address is best to reach you for questions about your repository?",
+        name: "email"
+    }
+  ])
+  .then((response) => {
+    var title = response.title;
+    var description = response.description;
+    var installation = response.installation;
+    var usage = response.usage;
+    var contribution = response.contribution;
+    var test = response.test;
+    var license = response.license;
+    var name = response.name;
+    var username = response.username;
+    var email = response.email;
+
+
+    var badging = licenseImage.get(license);
+
+    var fileContents = 
+    `${badging}
+    # ${title}
+    ${description}
+    ## Table of Contents
+    [Installing/Dependencies] (#installing/dependencies)
+    [Walkthrough/Usage Information] (#installing/dependencies)
+    [Contributing to the README.md Generator] (#contributing-to-the-readme.md-generator)
+    [Test Instructions] (#test-instructions)
+    [Author] (#author)
+    [License] (#license)
+    ## Installing/Dependencies
+    \`\`\`${installation}\`\`\`
+    ## Walkthrough/Usage Information
+    ${usage}
+    ## Contributing to the README.md Generator
+    ${contribution}
+    ## Test Instructions
+    ${test}
+    ## Author
+    [${name}](https://github.com/${username}) -- You can reach me anytime for questions or collaboration at ${email}.
+    ## License
+    This project is licensed under [${license}](LICENSE) - 2020 Laura Baumann`
+
+    fs.writeFile('README.md', fileContents, (error) =>
+    error ? console.error(error) : console.log('Success!'));
+});
